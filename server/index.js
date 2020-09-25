4378959150865099f9b887a5b8c3b45c18c6c2d1
Bipin Singh <bipinsingh061@gmail.com>
2020-09-25 18:23:49 +0530

    added static folder and SPA

--- server/index.js
+++ server/index.js
@@ -13,6 +13,16 @@ const posts=require('./routes/api/posts');
 
 app.use('/api/posts' , posts);
 
+// handle production 
+
+if(process.env.NODE_ENV === 'production'){
+	// static folder
+
+	app.use(express.static(__dirname + '/public'));
+	// handle single page application
+	app.get(/.*/ , (req,res)=>res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000 ;
 
 app.listen(port , () => {
